156e6d6862a742cbd86d8c7e0588c50a806c6c79
LeAnh <sinhla213@gmail.com>
2024-01-23 11:39:59 +0700

    end of section 5

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -4,32 +4,41 @@ import { BehaviorSubject, map } from 'rxjs';
 import { User } from '../_models/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AccountService {
   baseUrl = 'https://localhost:5001/api/';
-  private currentUserSource = new BehaviorSubject<User|null>(null);
+  private currentUserSource = new BehaviorSubject<User | null>(null);
   currentUser$ = this.currentUserSource.asObservable();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  login(model:any){
-    return this.http.post<User>(this.baseUrl + 'account/login',model).pipe(
+  login(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
       map((response: User) => {
         const user = response;
-        if(user){
-          localStorage.setItem('user',JSON.stringify(user));
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
           this.currentUserSource.next(user);
         }
       })
     );
   }
 
-  setCurrentUser(user: User){
+  register(model: any){
+    return this.http.post<User>(this.baseUrl + 'account/register',model).pipe(map(user => {
+      if(user){
+        localStorage.setItem('user',JSON.stringify(user));
+        this.currentUserSource.next(user);
+      }
+    }))
+  }
+
+  setCurrentUser(user: User) {
     this.currentUserSource.next(user);
   }
-  
-  logout(){
+
+  logout() {
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
   }
